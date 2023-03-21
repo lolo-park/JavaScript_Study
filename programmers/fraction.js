ee5834cd6c22729daddc763070004f0c4d8c5cc2
JINJOO PARK <117656236+lolo-park@users.noreply.github.com>
2023-03-21 12:39:34 +0900

    분수 덧셈 ing

---
+++ programmers/fraction.js
@@ -0,0 +1,19 @@
+/*
+첫 번째 분수의 분자와 분모를 뜻하는 numer1, denom1, 
+두 번째 분수의 분자와 분모를 뜻하는 numer2, denom2가 매개변수로 주어집니다. 
+두 분수를 더한 값을 기약 분수로 나타냈을 때 
+분자와 분모를 순서대로 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+
+분수 덧셈에서 분모가 같아야한다. 
+
+1. if (denom1 === denom2) {
+
+}
+2. else {denom1과 denom2를 같게 만들어줘야하는데!}
+
+
+
+
+*/
+
+const solution = (numer1, denom1, number2, denom2) => {};
